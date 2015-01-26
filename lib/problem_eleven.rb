323edc47db3f4e1e1e6d360a14048808010ac75d
Lisa Hamm <lisalhamm@gmail.com>
2015-01-26 13:40:18 -0600

    Added adjacent_items_down method

--- lib/problem_eleven.rb
+++ lib/problem_eleven.rb
@@ -13,5 +13,20 @@ module ProjectEuler
       end
       combinations
     end
+
+    def self.adjacent_items_down(two_dimensional_array)
+      combinations = []
+      row_index = 0
+      while row_index < two_dimensional_array[0].length
+        column_index = 0
+        while column_index < two_dimensional_array.length - 3
+          combinations << [two_dimensional_array[column_index][row_index], two_dimensional_array[column_index+1][row_index], two_dimensional_array[column_index+2][row_index], two_dimensional_array[column_index+3][row_index]]
+          column_index += 1
+        end
+        row_index += 1
+      end
+      combinations
+    end
+
   end
 end
