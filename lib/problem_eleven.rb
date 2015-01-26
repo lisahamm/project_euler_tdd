7115a8febdae99d2d8e1c271216635561009d049
Lisa Hamm <lisalhamm@gmail.com>
2015-01-26 15:03:33 -0600

    Added adjacent_items_diagonal_up method

--- lib/problem_eleven.rb
+++ lib/problem_eleven.rb
@@ -2,25 +2,11 @@ module ProjectEuler
   module ProblemEleven
     def self.adjacent_items_left(two_dimensional_array)
       combinations = []
-      column_index = 0
-      while column_index < two_dimensional_array.length
-        row_index = 0
-        while row_index < two_dimensional_array[0].length - 3
-          combinations << [two_dimensional_array[column_index][row_index], two_dimensional_array[column_index][row_index+1], two_dimensional_array[column_index][row_index+2], two_dimensional_array[column_index][row_index+3]]
-          row_index += 1
-        end
-        column_index += 1
-      end
-      combinations
-    end
-
-    def self.adjacent_items_down(two_dimensional_array)
-      combinations = []
       row_index = 0
-      while row_index < two_dimensional_array[0].length
+      while row_index < two_dimensional_array.length
         column_index = 0
-        while column_index < two_dimensional_array.length - 3
-          combinations << [two_dimensional_array[column_index][row_index], two_dimensional_array[column_index+1][row_index], two_dimensional_array[column_index+2][row_index], two_dimensional_array[column_index+3][row_index]]
+        while column_index < two_dimensional_array[0].length - 3
+          combinations << [two_dimensional_array[row_index][column_index], two_dimensional_array[row_index][column_index+1], two_dimensional_array[row_index][column_index+2], two_dimensional_array[row_index][column_index+3]]
           column_index += 1
         end
         row_index += 1
@@ -28,5 +14,46 @@ module ProjectEuler
       combinations
     end
 
+    def self.adjacent_items_down(two_dimensional_array)
+      combinations = []
+      column_index = 0
+      while column_index < two_dimensional_array[0].length
+        row_index = 0
+        while row_index < two_dimensional_array.length - 3
+          combinations << [two_dimensional_array[row_index][column_index], two_dimensional_array[row_index+1][column_index], two_dimensional_array[row_index+2][column_index], two_dimensional_array[row_index+3][column_index]]
+          row_index += 1
+        end
+        column_index += 1
+      end
+      combinations
+    end
+
+    def self.adjacent_items_diagonal_down(two_dimensional_array)
+      combinations = []
+      column_index = 0
+      while column_index < two_dimensional_array.length - 3
+        row_index = 0
+        while row_index < two_dimensional_array[0].length - 3
+          combinations << [two_dimensional_array[column_index][row_index], two_dimensional_array[column_index+1][row_index+1], two_dimensional_array[column_index+2][row_index+2], two_dimensional_array[column_index+3][row_index+3]]
+          row_index += 1
+        end
+        column_index += 1
+      end
+      combinations
+    end
+
+    def self.adjacent_items_diagonal_up(two_dimensional_array)
+      combinations = []
+      row_index = two_dimensional_array.length-1
+      while row_index >= 3
+        column_index = 0
+        while column_index < two_dimensional_array[0].length-3
+          combinations << [two_dimensional_array[row_index][column_index], two_dimensional_array[row_index-1][column_index+1], two_dimensional_array[row_index-2][column_index+2], two_dimensional_array[row_index-3][column_index+3]]
+          column_index += 1
+        end
+        row_index -= 1
+      end
+      combinations
+    end
   end
 end
--- spec/problem_eleven_spec.rb
+++ spec/problem_eleven_spec.rb
@@ -3,6 +3,10 @@ require 'problem_eleven'
 
 module ProjectEuler
   describe ProblemEleven do
+
+    let (:four_by_four_array) {[[1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16]]}
+    let (:five_by_five_array) {[[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15], [16,17,18,19,20], [21,22,23,24,25]]}
+
     it "returns all combinations of 4 adjacent items in the left direction in a 2 dimensional array" do
       expect(ProblemEleven.adjacent_items_left([[1,2,3,4]])).to eq [[1,2,3,4]]
     end
@@ -15,5 +19,16 @@ module ProjectEuler
       expect(ProblemEleven.adjacent_items_down([[1,2],[3,4],[5,6],[7,8]])).to eq [[1,3,5,7],[2,4,6,8]]
     end
 
+    it "returns all combinations of 4 adjacent items in the downward diagonal direction in a 2 dimensional array" do
+      expect(ProblemEleven.adjacent_items_diagonal_down(four_by_four_array)).to eq [[1,6,11,16]]
+    end
+
+    it "returns all combinations of 4 adjacent items in the downward diagonal direction in a 2 dimensional array" do
+      expect(ProblemEleven.adjacent_items_diagonal_down(five_by_five_array)).to eq [[1,7,13,19], [2,8,14,20], [6,12,18,24], [7,13,19,25]]
+    end
+
+    it "returns all combinations of 4 adjacent items in the upward diagonal direction in a 2 dimensional array" do
+      expect(ProblemEleven.adjacent_items_diagonal_up(five_by_five_array)).to eq [[21,17,13,9], [22,18,14,10], [16,12,8,4], [17,13,9,5]]
+    end
   end
 end
