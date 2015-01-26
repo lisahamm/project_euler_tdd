5d36d26337abfeefa7d3adf0c1cebf0ffd1d2345
Lisa Hamm <lisalhamm@gmail.com>
2015-01-26 12:13:54 -0600

    Added partial solution for problem 11

--- lib/problem_eleven.rb
+++ lib/problem_eleven.rb
@@ -1,5 +1,17 @@
 module ProjectEuler
   module ProblemEleven
-
+    def self.adjacent_items_left(two_dimensional_array)
+      combinations = []
+      column_index = 0
+      while column_index < two_dimensional_array.length
+        row_index = 0
+        while row_index < two_dimensional_array[0].length - 3
+          combinations << [two_dimensional_array[column_index][row_index], two_dimensional_array[column_index][row_index+1], two_dimensional_array[column_index][row_index+2], two_dimensional_array[column_index][row_index+3]]
+          row_index += 1
+        end
+        column_index += 1
+      end
+      combinations
+    end
   end
 end
--- spec/problem_eleven_spec.rb
+++ spec/problem_eleven_spec.rb
@@ -4,7 +4,11 @@ require 'problem_eleven'
 module ProjectEuler
   describe ProblemEleven do
     it "returns all combinations of 4 adjacent items in the left direction in a 2 dimensional array" do
-      expect(ProblemEleven.adjacent_items_left([[1]])).to eq [1]
+      expect(ProblemEleven.adjacent_items_left([[1,2,3,4]])).to eq [[1,2,3,4]]
+    end
+
+    it "returns all combinations of 4 adjacent items in the left direction in a 2 dimensional array" do
+      expect(ProblemEleven.adjacent_items_left([[1,2,3,4],[5,6,7,8]])).to eq [[1,2,3,4],[5,6,7,8]]
     end
   end
 end
