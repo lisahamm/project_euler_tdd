d0c02f0d0e1413a571e3cfa55ef76f995a1697a0
Lisa Hamm <lisalhamm@gmail.com>
2015-02-02 14:37:38 -0500

    Added solution to problem 15

--- lib/problem_fifteen.rb
+++ lib/problem_fifteen.rb
@@ -1,15 +1,23 @@
 module ProjectEuler
   module ProblemFifteen
-    def self.required_number_of_moves(length, width)
-      length + width
+    def self.required_number_of_moves(rows, columns)
+      rows + columns
     end
 
-    def self.max_num_of_right_moves(width)
-      width
+    def self.number_of_right_moves(columns)
+      columns
     end
 
-    def self.max_num_of_down_moves(length)
-      length
+    def self.number_of_down_moves(rows)
+      rows
+    end
+
+    def self.factorial_of(number)
+      (1..number).reduce(:*)
+    end
+
+    def self.number_of_possible_routes(rows, columns)
+      factorial_of(rows + columns)/(factorial_of(rows)*factorial_of(columns))
     end
   end
 end
--- spec/problem_fifteen_spec.rb
+++ spec/problem_fifteen_spec.rb
@@ -12,11 +12,42 @@ module ProjectEuler
     end
 
     it "returns the maximum number of right moves" do
-      expect(ProblemFifteen.max_num_of_right_moves(1)).to eq 1
+      expect(ProblemFifteen.number_of_right_moves(1)).to eq 1
     end
 
     it "returns the maximum number of downward moves" do
-      expect(ProblemFifteen.max_num_of_down_moves(1)).to eq 1
+      expect(ProblemFifteen.number_of_down_moves(1)).to eq 1
     end
+
+    it "returns 1 for the factorial of 1" do
+      expect(ProblemFifteen.factorial_of(1)).to eq 1
+    end
+
+    it "returns 2 for the factorial of 2" do
+      expect(ProblemFifteen.factorial_of(2)).to eq 2
+    end
+
+    it "returns 6 for the factorial of 3" do
+      expect(ProblemFifteen.factorial_of(3)).to eq 6
+    end
+
+    it "returns 2 as the total number of possible routes for a 1 x 1 grid" do
+      expect(ProblemFifteen.number_of_possible_routes(1,1)).to eq 2
+    end
+
+    it "returns 6 as the total number of possible routes for a 2 x 2 grid" do
+      expect(ProblemFifteen.number_of_possible_routes(2,2)).to eq 6
+    end
+
+    it "returns 20 as the total number of possible routes for a 3 x 3 grid" do
+      expect(ProblemFifteen.number_of_possible_routes(3,3)).to eq 20
+    end
+
+    it "returns 137846528820 as the total number of possible routes for a 20 x 20 grid" do
+      expect(ProblemFifteen.number_of_possible_routes(20,20)).to eq 137846528820
+    end
+
+
+
   end
 end
