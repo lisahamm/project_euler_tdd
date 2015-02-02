4cd378b67814bad2cc43f5cb08254adeb0238ab0
Lisa Hamm <lisalhamm@gmail.com>
2015-02-02 15:54:47 -0500

    Added solution to problem 16

--- lib/problem_sixteen.rb
+++ lib/problem_sixteen.rb
@@ -1,5 +1,15 @@
 module ProjectEuler
   module ProblemSixteen
+    def self.base_raised_to_power(base, power)
+      base ** power
+    end
 
+    def self.digits_of(number)
+      number.to_s.chars.map(&:to_i)
+    end
+
+    def self.sum_of_array(number_array)
+      number_array.reduce(:+)
+    end
   end
 end
--- spec/problem_sixteen_spec.rb
+++ spec/problem_sixteen_spec.rb
@@ -3,6 +3,45 @@ require 'problem_sixteen'
 
 module ProjectEuler
   describe ProblemSixteen do
+    it "returns the product of a base raised to a power" do
+      expect(ProblemSixteen.base_raised_to_power(1,1)).to eq 1
+    end
 
+    it "returns the product of a base raised to a power" do
+      expect(ProblemSixteen.base_raised_to_power(2,2)).to eq 4
+    end
+
+    it "returns the product of a base raised to a power" do
+      expect(ProblemSixteen.base_raised_to_power(1,1)).to eq 1
+    end
+
+    it "returns an array of a number's digits" do
+      expect(ProblemSixteen.digits_of(1)).to eq [1]
+    end
+
+    it "returns an array of a number's digits" do
+      expect(ProblemSixteen.digits_of(11)).to eq [1,1]
+    end
+
+    it "returns an array of a number's digits" do
+      expect(ProblemSixteen.digits_of(1234)).to eq [1,2,3,4]
+    end
+
+    it "returns the sum of all digits in an array" do
+      expect(ProblemSixteen.sum_of_array([1])).to eq 1
+    end
+
+    it "returns the sum of all digits in an array" do
+      expect(ProblemSixteen.sum_of_array([1,1])).to eq 2
+    end
+
+    it "returns the sum of all digits in an array" do
+      expect(ProblemSixteen.sum_of_array([1,1,2,4])).to eq 8
+    end
+
+    it "returns the sum of the digits of 2^1000" do
+      number = ProblemSixteen.base_raised_to_power(2, 1000)
+      expect(ProblemSixteen.sum_of_array(ProblemSixteen.digits_of(number))).to eq 1366
+    end
   end
 end
